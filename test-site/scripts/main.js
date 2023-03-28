3dac8c4ad5481fc279227cb206c378b9f7bb4ff9
takeroaoki <103397727+takeroaoki@users.noreply.github.com>
2023-03-28 17:10:05 +0900

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,39 @@
+const myImage = document.querySelector('img');
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/クロ.jpg') {
+        myImage.setAttribute('src','images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/クロ.jpg');
+
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+
+function setUserName() {
+    const myName = prompt('あなたの名前を入力してください。');
+    if (!myName) {
+        setUserName();
+    } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Mozilla はかっこいいよ、${myName}`;
+  }
+}
+  
+  if (!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem('name');
+    myHeading.textContent = `Mozilla はかっこいいよ、${storedName}`;
+  }
+  
+
+
+  myButton.onclick = () => {
+    setUserName();
+  }
+  
